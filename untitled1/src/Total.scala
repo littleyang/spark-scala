b42b61f8d22d0b7dac2e1f223db6aa8f6b77e9d8
littleyang <littleyangqh@gmail.com>
2016-05-25 18:19:37 +0800

    dd

--- untitled1/src/Total.scala
+++ untitled1/src/Total.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.SparkContext._
 import org.apache.spark.{SparkConf, SparkContext}
+import java.util.{Date, Locale}
 
 /**
   * Created by yangyang on 5/25/16.
@@ -7,11 +8,10 @@ import org.apache.spark.{SparkConf, SparkContext}
 object Total {
 
   def main(args: Array[String]) {
-    val logFile = "hdfs://10.0.58.21:9000/nginx/2016/05/24/*.log" // Should be some file on your system
+    val logFile = "hdfs://10.0.58.21:9000/nginx/2016/05/24/*.log"
     val conf = new SparkConf().setAppName("Simple Application").setMaster("spark://10.0.58.21:7077")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile)
     println(logData.count())
   }
-
 }
