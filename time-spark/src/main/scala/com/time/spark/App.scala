8166cf1e50c9f1b96be288d8df6d51f88e6e9bb0
littleyang <littleyangqh@gmail.com>
2016-05-26 10:39:47 +0800

    fix bugs

--- time-spark/src/main/scala/com/time/spark/App.scala
+++ time-spark/src/main/scala/com/time/spark/App.scala
@@ -1,9 +1,17 @@
 package com.time.spark
 
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
  * Hello world!
  *
  */
-object App extends Application {
-  println( "Hello World!" )
+object App {
+  def main(args: Array[String]) {
+    val logFile = "hdfs://10.0.58.21:9000/nginx/2016/05/24/*.log"
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("spark://10.0.58.21:7077")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile)
+    println(logData.count())
+  }
 }
